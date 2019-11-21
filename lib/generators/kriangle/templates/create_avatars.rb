50f75013a5a30f8d9231f85b7eb18f9ccc977fd0
Lalit Kumar Maurya <lalit.maurya@engineer.ai>
2019-11-21 09:10:51 +0530

    [Bug] Migration Issues

--- lib/generators/kriangle/templates/create_avatars.rb
+++ lib/generators/kriangle/templates/create_avatars.rb
@@ -1,7 +1,7 @@
 class CreateAvatars < ActiveRecord::Migration<%= "[#{Rails::VERSION::STRING[0..2]}]" if Rails::VERSION::MAJOR > 4 %>
   def change
     create_table :avatars do |t|
-      t.references :<%= underscored_user_class %>
+      t.references :<%= underscored_user_class %>, foreign_key: true
 
       t.text :image
       t.integer :sorting, default: 0
--- lib/generators/kriangle/templates/module_migration.rb
+++ lib/generators/kriangle/templates/module_migration.rb
@@ -2,7 +2,11 @@ class Create<%= class_name.pluralize %> < ActiveRecord::Migration[5.2]
   def change
     create_table :<%= controller_file_name %> do |t|
       <%- for ma in model_associations.select { |ma| ma.association_type == 'belongs_to' }.uniq { |ma| ma.association_name } -%>
-      t.references :<%= ma.association_name %>, foreign_key: true
+        <%- if ma.foreign_key.present? -%>
+      t.integer :<%= ma.foreign_key %>, index: true
+        <%- else -%>
+      t.references :<%= ma.association_name %><%= ", foreign_key: true" if ma.class_name.blank? %>
+        <%- end -%>
       <%- end -%>
       <%- if self_reference -%>
       t.references :<%= parent_association_name %>, foreign_key: true
@@ -14,7 +18,6 @@ class Create<%= class_name.pluralize %> < ActiveRecord::Migration[5.2]
       <%- for attribute in @attributes -%>
       t.<%= attribute.type || 'string'  %> :<%= attribute.name %><%= ", default: #{attribute.default.gsub('~', "'")}" unless attribute.default.nil? %>
       <%- end -%>
-
       <%- unless skip_timestamps -%>
       t.timestamps
       <%- end -%>
